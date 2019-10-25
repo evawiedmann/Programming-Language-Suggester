b2efed7b29241ac47e73cf1d725fb27666a888e9
Eva Wiedmann <evawiedmann@gmail.com>
2019-10-25 10:41:44 -0700

    add first split point

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $("form#selection1").submit(function(event) {
+    var wordsIn = $("select#selection1").val();
+var result;
+if (wordsIn === "Words"){
+  result = show.("#selection21");
+} else {
+  result = show.(q22)
+}
+    $("#quote").show();
+
+    event.preventDefault();
+  });
 });
