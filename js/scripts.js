4b75d8612e88947bfffbb5f23061b24f9dd22106
Eva Wiedmann <evawiedmann@gmail.com>
2019-10-25 13:30:58 -0700

    code does not work, but adding in more answers to try to meet requirements as much as possible

--- js/scripts.js
+++ js/scripts.js
@@ -5,34 +5,45 @@ $(document).ready(function() {
     if (love = yes) {
       $("#lucky").show();
     } else {
-      var colornum = prompt("Do you prefer colors or letters?")
-      if (colorletter = colors){
+      var colorletter = prompt("Do you prefer colors or letters?")
+      if (colorletter = "colors"){
         var morecolors = prompt("Do you prefer jewel tones or earth tones?")
         if (morecolors = earth tones) {
+          $("#rust").show();
+        } else {
+          $("#ruby").show();
+        }
+      } else {
+        var letter = prompt("Do you like the first or second half of the alphabet more?")
+        if (letter == a ||  letter == b || letter == c || letter == d || letter == e || letter == f || letter == g || letter == h || letter == i || letter == j || letter == k || letter == l || letter == m)
+        var student = prompt("Where you a good student in grade school?")
+        if (student = yes) {
+          $("#aplus").show();
         } else {
 
         }
       } else {
-        var letters = prompt("Do you like the first or second half of the alphabet more?")
+
       }
-    }} else {
-
-      $("#young").show();
     }
+  }} else {
+
+    $("#young").show();
+  }
 
 
 
-    // }
-    // if (age >= 5) {
-      //   $("#older").prompt("Do you prefer initialisms or words?")
-      //   var wordsIn = prompt("Do you prefer initialisms or words?")
-      //   if (wordsIn = words){
-        //     $("#words").prompt("Do you prefer colors or animals?")
-        //   } else {
-          //     $("#initialisms").prompt("Choose a number:")
-          //     var number = prompt("Choose a number:")
-          //     if (number >= 14) {
-            //       $(#other)
-            //
+  // }
+  // if (age >= 5) {
+    //   $("#older").prompt("Do you prefer initialisms or words?")
+    //   var wordsIn = prompt("Do you prefer initialisms or words?")
+    //   if (wordsIn = words){
+      //     $("#words").prompt("Do you prefer colors or animals?")
+      //   } else {
+        //     $("#initialisms").prompt("Choose a number:")
+        //     var number = prompt("Choose a number:")
+        //     if (number >= 14) {
+          //       $(#other)
+          //
 
-          });
+        });
