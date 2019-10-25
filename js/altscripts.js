49ef12b7e753f42717d68a79b28461acf24c0859
Eva Wiedmann <evawiedmann@gmail.com>
2019-10-25 16:22:35 -0700

    make some progress, knowing that it won't work for now, but I know I'm just a couple steps away

--- js/altscripts.js
+++ js/altscripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
+
+  $("form#wordsOptions").submit(function(event){
+    event.preventDefault();
+    var item = $("#words").val();
+    if (item = 1) {
+      $("#information").show();
+    } else {
+      $("#alphabet").show();
+    }
+  });
+
+
   $("form#information").submit(function(event){
     event.preventDefault();
     var item = $("#information").val();
     if (item = 1) {
-      $("#lucky").show();
+      $("#animalOptions").show();
     } else {
       $("#colorOptions").show();
     }
