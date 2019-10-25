c925192d1b3f7484084d1fc241b3e51fa466faca
Eva Wiedmann <evawiedmann@gmail.com>
2019-10-25 13:10:21 -0700

    nest prompts, more hair-pulling

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
-prompt("How old are you?");
+$(document).ready(function() {
+  var age = parseInt(prompt("How old are you?"));
+  if (age >= 5) {
+    var love = prompt("Do you already love programming?")
+    if (love = yes) {
+      $("#lucky").show();
+    } else {
+      var colornum = prompt("Do you prefer colors or letters?")
+      if (colorletter = colors){
+        var morecolors = prompt("Do you prefer jewel tones or earth tones?")
+        if (morecolors = earth tones) {
+        } else {
+
+        }
+      } else {
+        var letters = prompt("Do you like the first or second half of the alphabet more?")
+      }
+    }} else {
+
+      $("#young").show();
+    }
 
 
-// var age = parseInt(prompt("How old are you?"));
-  // if (age >= 5) {
-    // $("#older").prompt("Do you prefer initialisms or words?")
-    // var wordsIn = prompt("Do you prefer initialisms or words?")
-    // if (wordsIn = words){
-//       $("#words").prompt("Do you prefer colors or animals?")
-//     } else {
-//     $("#initialisms").prompt("Choose a number:")
-//     var number = prompt("Choose a number:")
-//     if (number >= 14) {
-//       $(#other)
-//
-//
-// });
+
+    // }
+    // if (age >= 5) {
+      //   $("#older").prompt("Do you prefer initialisms or words?")
+      //   var wordsIn = prompt("Do you prefer initialisms or words?")
+      //   if (wordsIn = words){
+        //     $("#words").prompt("Do you prefer colors or animals?")
+        //   } else {
+          //     $("#initialisms").prompt("Choose a number:")
+          //     var number = prompt("Choose a number:")
+          //     if (number >= 14) {
+            //       $(#other)
+            //
+
+          });
