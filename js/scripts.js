3e11fcb43422934825c4a188c1a433658cf94800
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-03 15:50:14 -0800

    rework framework, parseInt input values, add together

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,21 @@
-$(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
-  if (age >= 5) {
-    var love = prompt("Do you already love programming?")
-    if (love = yes) {
-      $("#lucky").show();
-    } else {
-      var colorletter = prompt("Do you prefer colors or letters?")
-      if (colorletter = "colors"){
-        var morecolors = prompt("Do you prefer jewel tones or earth tones?")
-        if (morecolors = earth tones) {
-          $("#rust").show();
-        } else {
-          $("#ruby").show();
-        }
-      } else {
-        var letter = prompt("Do you like the first or second half of the alphabet more?")
-        if (letter == a ||  letter == b || letter == c || letter == d || letter == e || letter == f || letter == g || letter == h || letter == i || letter == j || letter == k || letter == l || letter == m)
-        var student = prompt("Where you a good student in grade school?")
-        if (student = yes) {
-          $("#aplus").show();
-        } else {
-          var coffee = prompt("Do you need some coffee?")
-          if (coffee = yes) {
-            $("#java").show();
-          } else {
-            var animal = prompt("Do you prefer birds or reptiles?")
-            if (animal = birds) {
-              $("#swift").show();
-            }
-          }
-        }
-      }
-    }
-  } else {
 
-    $("#young").show();
-  }
+
+$(document).ready(function() {
+  $("form#questions").submit(function(event){
+    event.preventDefault();
+    $("#questions").hide();
+    var colorVal = parseInt($("select#color").val());
+    var foodVal = parseInt($("select#food").val());
+    var sportVal = parseInt($("select#sport").val());
+    var animalVal = parseInt($("select#animal").val());
+    var weatherVal = parseInt($("select#weather").val());
+    var userInputVal = (colorVal+foodVal+sportVal+animalVal+weatherVal);
+    if (userInputVal > 10) {
+      $("#python").show();
+    } else if (userInputVal > 5) {
+      $("#java").show();
+    } else {
+      $("#ruby").show();
+    }
+  });
 });
